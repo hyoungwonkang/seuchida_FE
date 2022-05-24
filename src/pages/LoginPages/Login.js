50900219f7fbde4de750cf2f14e8c419c6be6a8b
hyoungwonkang <hwplus@gmail.com>
2022-05-24 22:45:45 +0900

    카카오, 구글 로그인 디자인 수정

--- src/pages/LoginPages/Login.js
+++ src/pages/LoginPages/Login.js
@@ -5,30 +5,33 @@ import { Grid, Button, Image, Text } from "../../elements/Index";
 
 const Login = (props) => {
   return (
-    <Grid column height="100vh" bg="#00D282">
-      {/* <Grid height="auto" column margin="250px 0px 50px 0px"> */}
-      <Grid column width="200px" height="200px" margin="auto">
-        <img src="./img/login.png" style={{ margin: "150px 0px" }} />
+    <Grid height="100vh" bg="#00D282">
+      <Grid column>
+        {/* <Grid height="auto" column margin="250px 0px 50px 0px"> */}
+        <Grid column width="200px" height="200px" margin="auto">
+          <img src="./img/login.png" style={{ margin: "150px 0px" }} />
+        </Grid>
+        <div style={{ margin: "250px 0px 20px 0px" }}>
+          <A
+            href={KAKAO_AUTH_URL}
+            onClick={() => {
+              window.location.href = "/main";
+            }}
+          >
+            <img src="./img/kakao_login_medium_wide.png" alt="카카오로그인" />
+          </A>
+        </div>
+        <GoogleBtn
+          style={{ display: "flex", justifyContent: "initial" }}
+          onClick={() => {
+            window.location.href = "/main";
+          }}
+        >
+          <img src="./img/btn_google_light_normal_ios.svg" alt="구글로그인" />
+          <Agoogle href={GOOGLE_AUTH_URL}>구글 로그인</Agoogle>
+        </GoogleBtn>
+        {/* </Grid> */}
       </Grid>
-      <Button
-        bg="#FDE333"
-        margin="250px 0px 0px 0px"
-        _onClick={() => {
-          window.location.href = "/main";
-        }}
-      >
-        <A href={KAKAO_AUTH_URL}>카카오 로그인</A>
-      </Button>
-      <Button
-        bg="red"
-        margin="20px 0px 50px 0px"
-        _onClick={() => {
-          window.location.href = "/main";
-        }}
-      >
-        <A href={GOOGLE_AUTH_URL}>구글 로그인</A>
-      </Button>
-      {/* </Grid> */}
     </Grid>
   );
 };
@@ -37,8 +40,29 @@ export default Login;
 
 const A = styled.a`
   text-decoration-line: none;
-  color: white;
   display: block;
-  height: 40px;
-  padding-top: 6px;
+  /* padding-top: 0px; */
+  font-weight: bold;
+  font-size: 14px;
+`;
+
+const Agoogle = styled.a`
+  margin-left: 78px;
+  margin-top: 13px;
+  text-decoration-line: none;
+  display: block;
+  /* padding-top: 0px; */
+  font-weight: bold;
+  font-size: 14px;
+`;
+
+const GoogleBtn = styled.button`
+  display: inline-block;
+  width: 300px;
+  height: 44px;
+  background: white;
+  border: none;
+  border-radius: 5px;
+  margin-bottom: 50px;
+  padding-left: 0px;
 `;
