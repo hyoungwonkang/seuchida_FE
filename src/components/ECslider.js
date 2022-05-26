ee73221963d6bf0076119aa9f5f57012cfa85cda
hyoungwonkang <hwplus@gmail.com>
2022-05-26 16:15:42 +0900

    머지 전 수정

--- src/components/ECslider.js
+++ src/components/ECslider.js
@@ -22,9 +22,9 @@ function ECslider(props) {
   return (
     <Sliders {...settings} style={{ height: "180px" }}>
       {myEx?.map((p, i) => {
-        // if (i < 5) {
-        return <EndCard {...p} key={p?.id} _onClick={props._onClick} />;
-        // }
+        if (i < 5) {
+          return <EndCard {...p} key={p?.id} _onClick={props._onClick} />;
+        }
       })}
     </Sliders>
   );
